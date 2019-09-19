3a6a631ec48009f3bbacb390fbd5ec2885371b2e
Sergey <sergeyshaykhullin@gmail.com>
2019-09-19 23:55:23 +0300

    Clean frontend

--- src/DocumentIO.Frontend/src/index.tsx
+++ src/DocumentIO.Frontend/src/index.tsx
@@ -1,15 +1,10 @@
+import 'semantic-ui-css/semantic.min.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import './index.scss'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
-import 'semantic-ui-css/semantic.min.css'
-import ApolloClient from 'apollo-boost'
 
-const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io'
-})
+import App from './App'
 
 ReactDOM.render(
   <BrowserRouter>
@@ -17,8 +12,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
