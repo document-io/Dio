6655bc8ada462da7d21d71195235cf6543d611d5
Sergey <sergeyshaykhullin@gmail.com>
2019-09-20 00:47:46 +0300

    Update package.json

--- src/DocumentIO.Frontend/src/index.tsx
+++ src/DocumentIO.Frontend/src/index.tsx
@@ -1,8 +1,7 @@
-import 'semantic-ui-css/semantic.min.css'
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css'
 
 import App from './App'
 
