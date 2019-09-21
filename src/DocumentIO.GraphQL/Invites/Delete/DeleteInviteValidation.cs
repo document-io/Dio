190e87648d881d1e39c2100d5e3687db60d0424b
sergeyshaykhullin <46970457+sergeyshaykhullin@users.noreply.github.com>
2019-09-21 20:03:27 +0300

    Переписал валидацию (#36)
    * Переписал валидацию
    
    * Добавил валидацию удаления приглашений

---
+++ src/DocumentIO.GraphQL/Invites/Delete/DeleteInviteValidation.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Phema.Validation;
+
+namespace DocumentIO
+{
+	public class DeleteInviteValidation : IDocumentIOValidation<object>
+	{
+		private readonly DatabaseContext databaseContext;
+
+		public DeleteInviteValidation(DatabaseContext databaseContext)
+		{
+			this.databaseContext = databaseContext;
+		}
+	
+		public async Task Validate(DocumentIOResolveFieldContext<object> context, IValidationContext validationContext)
+		{
+			var accountId = context.GetAccountId();
+			var inviteId = context.GetArgument<Guid>("id");
+
+			var invite = await databaseContext
+				.Invites
+				.Where(x => x.Organization.Accounts.Any(account => account.Id == accountId))
+				.FirstOrDefaultAsync(x => x.Id == inviteId);
+
+			validationContext.When("id")
+				.Is(() => invite == null)
+				.AddError("Приглашение не найдено");
+
+			if (validationContext.IsValid("id"))
+			{
+				validationContext.When("id")
+					.Is(() => invite.AccountId != null)
+					.AddError("Нельзя удалить использованное приглашение");
+			}
+		}
+	}
+}
