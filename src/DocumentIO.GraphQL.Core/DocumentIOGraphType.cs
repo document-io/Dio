09a5e8ae019607a77fad00e51b647a99cf836132
Sergey <sergeyshaykhullin@gmail.com>
2019-09-22 15:01:54 +0300

    Добавил версию в граф

--- src/DocumentIO.GraphQL.Core/DocumentIOGraphType.cs
+++ src/DocumentIO.GraphQL.Core/DocumentIOGraphType.cs
@@ -6,13 +6,13 @@ namespace DocumentIO
 	public class DocumentIOGraphType<TSourceType> : ObjectGraphType<TSourceType>
 	{
 		protected DocumentIOFieldBuilder<TSourceType, TReturnType> DocumentIOField<TGraphType, TReturnType>(string name)
-			where TGraphType : ComplexGraphType<TReturnType>
+			where TGraphType : GraphType
 		{
 			return new DocumentIOFieldBuilder<TSourceType, TReturnType>(Field<TGraphType, TReturnType>(name));
 		}
 
 		protected DocumentIOFieldBuilder<TSourceType, IEnumerable<TReturnType>> DocumentIOListField<TGraphType, TReturnType>(string name)
-			where TGraphType : ComplexGraphType<TReturnType>
+			where TGraphType : GraphType
 		{
 			return new DocumentIOFieldBuilder<TSourceType, IEnumerable<TReturnType>>(
 				Field<ListGraphType<TGraphType>, IEnumerable<TReturnType>>(name));
