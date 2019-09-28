e1aecdfef2734d3d256222ee720da2b8be4d7158
Sergey <sergeyshaykhullin@gmail.com>
2019-09-28 21:59:58 +0300

    Табы для дашборда

--- src/DocumentIO.Frontend/src/pages/dashboard/index.tsx
+++ src/DocumentIO.Frontend/src/pages/dashboard/index.tsx
@@ -1,12 +1,46 @@
 import React from "react"
+import { Grid, Tab } from "semantic-ui-react"
+
 import { DocumentIOMenu } from "../../components/menu"
+import { DashboardBoardsTab } from "./boards"
+import { DashboardUsersTab } from "./users"
+import { DashboardInviteTab } from "./invites"
+
 
 export const DashboardPage = () => {
 	return (
 		<React.Fragment>
-			<DocumentIOMenu logoUrl='/dashboard' search dropdown />
+			<DocumentIOMenu logoUrl='/dashboard' search dropdown/>
 
-			<div>Dashboard</div>
+			<Grid centered>
+				<Grid.Column textAlign='center'>
+					<Tab menu={ {pointing: true, compact: true} } panes={ panes }/>
+				</Grid.Column>
+			</Grid>
 		</React.Fragment>
 	)
 }
+
+const panes = [
+	{
+		menuItem: {key: 'boards', icon: 'table', content: 'Доски'},
+		render: () => (
+			<Tab.Pane attached={ false }>
+				<DashboardBoardsTab/>
+			</Tab.Pane>),
+	},
+	{
+		menuItem: {key: 'users', icon: 'users', content: 'Пользователи'},
+		render: () => (
+			<Tab.Pane attached={ false }>
+				<DashboardUsersTab/>
+			</Tab.Pane>),
+	},
+	{
+		menuItem: {key: 'invites', icon: 'sitemap', content: 'Приглашения'},
+		render: () => (
+			<Tab.Pane attached={ false }>
+				<DashboardInviteTab/>
+			</Tab.Pane>),
+	},
+]
