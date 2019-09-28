10b59053d20692332a43c08f6030d8bd3f22f6f8
Sergey <sergeyshaykhullin@gmail.com>
2019-09-28 17:43:21 +0300

    Loading, succeess, /graphql

--- src/DocumentIO.Frontend/src/pages/create/content.tsx
+++ src/DocumentIO.Frontend/src/pages/create/content.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent, useState } from "react"
-import { Button, Form, Grid, Header } from "semantic-ui-react"
+import { Button, Form, Grid, Header, Message } from "semantic-ui-react"
 
 import { ApolloError } from 'apollo-boost'
 import { useMutation } from "@apollo/react-hooks"
@@ -8,6 +8,9 @@ import { CreateOrganization, CreateOrganizationVariables } from "./mutations"
 import { RouteComponentProps } from "react-router"
 
 export const CreatePageContent = (props: RouteComponentProps) => {
+	const [loading, setLoading] = useState(false)
+	const [success, setSuccess] = useState(false)
+
 	const [name, setName] = useState<string>("")
 	const [nameValidation, setNameValidation] = useState<string>("")
 
@@ -35,6 +38,8 @@ export const CreatePageContent = (props: RouteComponentProps) => {
 	const onSubmit = (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault()
 
+		setLoading(true)
+
 		createOrganization({
 			variables: {
 				organization: {
@@ -42,14 +47,17 @@ export const CreatePageContent = (props: RouteComponentProps) => {
 					accounts: [{
 						login,
 						email,
-						password,
+						password: password === confirmPassword ? password : '',
 						lastName,
 						firstName
 					}]
 				}
 			}
 		})
-			.then(value => props.history.push('/login'))
+			.then(value => {
+				setSuccess(true)
+				setTimeout(() => props.history.push('/login'), 200)
+			})
 			.catch((reason: ApolloError) => {
 				reason.graphQLErrors
 					.forEach(error => {
@@ -70,7 +78,9 @@ export const CreatePageContent = (props: RouteComponentProps) => {
 								break;
 
 							case 'accounts[0].password':
-								setPasswordValidation(message)
+								if (password === confirmPassword) {
+									setPasswordValidation(message)
+								}
 								break;
 
 							case 'accounts[0].firstName':
@@ -84,80 +94,84 @@ export const CreatePageContent = (props: RouteComponentProps) => {
 					})
 
 				if (password !== confirmPassword) {
-					setConfirmPasswordValidation("Пароли не совпадают")
+					const message = "Пароли не совпадают"
+
+					setPasswordValidation(message)
+					setConfirmPasswordValidation(message)
 				}
+
+				setLoading(false)
 			})
 	}
 
 	return (
 		<Grid centered columns='2'>
 			<Grid.Column>
-				<Form /*loading*/ onSubmit={ onSubmit }>
-					<Form.Field>
-						<Grid>
-							<Grid.Column textAlign='center'>
-								<Header>Создание организации</Header>
-							</Grid.Column>
-						</Grid>
-					</Form.Field>
 
+				{success ? (
+					<Message
+						success
+						header='Организация создана'
+						content={ `Организация '${ name }' успешно создана` } />) : null}
+
+				<Form loading={ loading } onSubmit={ onSubmit }>
 					<Form.Input
-						error={nameValidation === "" ? null : { content: nameValidation }}
+						error={ nameValidation === "" ? null : {content: nameValidation} }
 						icon='building'
 						iconPosition='left'
 						placeholder='Введите название организации'
 						onChange={ (event, data) => {
 							setName(data.value)
 							setNameValidation("")
-						}}
+						} }
 					/>
 
 					<Form.Input
-						error={loginValidation === "" ? null : { content: loginValidation }}
+						error={ loginValidation === "" ? null : {content: loginValidation} }
 						icon='bullhorn'
 						iconPosition='left'
 						placeholder='Введите ваш логин'
 						onChange={ (event, data) => {
 							setLogin(data.value)
 							setLoginValidation("")
-						}}
+						} }
 					/>
 
 					<Form.Input
-						error={emailValidation === "" ? null : { content: emailValidation }}
+						error={ emailValidation === "" ? null : {content: emailValidation} }
 						icon='at'
 						iconPosition='left'
 						placeholder='Введите ваш email'
 						onChange={ (event, data) => {
 							setEmail(data.value)
 							setEmailValidation("")
-						}}
+						} }
 					/>
 
 					<Form.Input
-						error={firstNameValidation === "" ? null : { content: firstNameValidation }}
+						error={ firstNameValidation === "" ? null : {content: firstNameValidation} }
 						icon='address card'
 						iconPosition='left'
 						placeholder='Введите ваше имя'
 						onChange={ (event, data) => {
 							setFirstName(data.value)
 							setFirstNameValidation("")
-						}}
+						} }
 					/>
 
 					<Form.Input
-						error={lastNameValidation === "" ? null : { content: lastNameValidation }}
+						error={ lastNameValidation === "" ? null : {content: lastNameValidation} }
 						icon='address card outline'
 						iconPosition='left'
 						placeholder='Введите вашу фамилию'
 						onChange={ (event, data) => {
 							setLastName(data.value)
 							setLastNameValidation("")
-						}}
+						} }
 					/>
 
 					<Form.Input
-						error={passwordValidation === "" ? null : { content: passwordValidation }}
+						error={ passwordValidation === "" ? null : {content: passwordValidation} }
 						icon='low vision'
 						iconPosition='left'
 						placeholder='Введите ваш пароль'
@@ -165,11 +179,11 @@ export const CreatePageContent = (props: RouteComponentProps) => {
 							setPassword(data.value)
 							setPasswordValidation("")
 							setConfirmPasswordValidation("")
-						}}
+						} }
 					/>
 
 					<Form.Input
-						error={confirmPasswordValidation === "" ? null : { content: confirmPasswordValidation }}
+						error={ confirmPasswordValidation === "" ? null : {content: confirmPasswordValidation} }
 						icon='map signs'
 						iconPosition='left'
 						placeholder='Повторите ваш пароль'
@@ -177,7 +191,7 @@ export const CreatePageContent = (props: RouteComponentProps) => {
 							setConfirmPassword(data.value)
 							setConfirmPasswordValidation("")
 							setPasswordValidation("")
-						}}
+						} }
 					/>
 
 					<Grid>
