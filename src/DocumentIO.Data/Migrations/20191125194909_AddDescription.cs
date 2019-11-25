5571445bdcfef266bbcd8fda20283b0f96d33b85
sergeyshaykhullin <sergeyshaykhullin@gmail.com>
2019-11-25 22:49:16 +0300

    Добавил миграцию

---
+++ src/DocumentIO.Data/Migrations/20191125194909_AddDescription.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DocumentIO.Migrations
+{
+    public partial class AddDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Cards",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Cards");
+        }
+    }
+}
