1de01d6e0761652157833028fc388bf4b5b89264
sergeyshaykhullin <sergeyshaykhullin@gmail.com>
2019-12-02 22:27:20 +0300

    Добавил допольнительные фильтры

--- src/DocumentIO.GraphQL/Attachments/AttachmentsFilter.cs
+++ src/DocumentIO.GraphQL/Attachments/AttachmentsFilter.cs
@@ -7,6 +7,12 @@ namespace DocumentIO
 	public class AttachmentsFilter : DocumentIOFilter<CardAttachment>
 	{
 		public Guid? Id { get; set; }
+		public string Name { get; set; }
+		public string FileName { get; set; }
+		public string ContentType { get; set; }
+		public string ContentDisposition { get; set; }
+		public long? Length { get; set; }
+		public Guid? CardId { get; set; }
 
 		public override IQueryable<TPaginated> Filtered<TPaginated, TOrderBy>(
 			IQueryable<CardAttachment> queryable,
@@ -15,6 +21,24 @@ namespace DocumentIO
 		{
 			if (Id != null)
 				queryable = queryable.Where(attachment => attachment.Id == Id);
+
+			if (Name != null)
+				queryable = queryable.Where(attachment => attachment.Name == Name);
+
+			if (FileName != null)
+				queryable = queryable.Where(attachment => attachment.FileName == FileName);
+
+			if (ContentType != null)
+				queryable = queryable.Where(attachment => attachment.ContentType == ContentType);
+
+			if (ContentDisposition != null)
+				queryable = queryable.Where(attachment => attachment.ContentDisposition == ContentDisposition);
+
+			if (Length != null)
+				queryable = queryable.Where(attachment => attachment.Length == Length);
+
+			if (CardId != null)
+				queryable = queryable.Where(attachment => attachment.CardId == CardId);
 
 			return base.Filtered(queryable, query, orderBy);
 		}
--- src/DocumentIO.GraphQL/Attachments/AttachmentsFilterType.cs
+++ src/DocumentIO.GraphQL/Attachments/AttachmentsFilterType.cs
@@ -5,6 +5,12 @@ namespace DocumentIO
 		public AttachmentsFilterType()
 		{
 			NullField(x => x.Id);
+			NullField(x => x.Name);
+			NullField(x => x.FileName);
+			NullField(x => x.ContentType);
+			NullField(x => x.ContentDisposition);
+			NullField(x => x.Length);
+			NullField(x => x.CardId);
 		}
 	}
 }
